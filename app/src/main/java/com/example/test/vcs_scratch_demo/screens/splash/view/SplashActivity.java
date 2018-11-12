f0a908f78060c2c6a9140ddacb14715ec97b5cc2
Shivam Bansal <shivam.bansal@daffodilsw.com>
2018-11-12 15:33:21 +0530

    added progressbar to splash screen

--- app/src/main/java/com/example/test/vcs_scratch_demo/screens/splash/view/SplashActivity.java
+++ app/src/main/java/com/example/test/vcs_scratch_demo/screens/splash/view/SplashActivity.java
@@ -1,7 +1,8 @@
 package com.example.test.vcs_scratch_demo.screens.splash.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 import com.example.test.vcs_scratch_demo.R;
 
@@ -11,5 +12,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setIndeterminate(true);
     }
 }
