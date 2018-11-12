034718a481a5c725c45ade9f4bec19d5624a56b6
DESKTOP-HO7V4OL\daffodil-31 <shivam.bansal@daffodilsw.com>
2018-11-12 11:50:45 +0530

    created splash with mvp

---
+++ app/src/main/java/com/example/test/vcs_scratch_demo/screens/splash/view/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.test.vcs_scratch_demo.screens.splash.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.test.vcs_scratch_demo.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
