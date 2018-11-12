e38fc8aaef600ac653b53b04afcbd58f91b0b236
DESKTOP-HO7V4OL\daffodil-31 <shivam.bansal@daffodilsw.com>
2018-11-12 12:41:32 +0530

    added button to settings screen

--- app/src/main/java/com/example/test/vcs_scratch_demo/screens/settings/view/SettingsActivity.java
+++ app/src/main/java/com/example/test/vcs_scratch_demo/screens/settings/view/SettingsActivity.java
@@ -1,7 +1,8 @@
 package com.example.test.vcs_scratch_demo.screens.settings.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.example.test.vcs_scratch_demo.R;
 
@@ -11,5 +12,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button button = findViewById(R.id.button);
     }
 }
